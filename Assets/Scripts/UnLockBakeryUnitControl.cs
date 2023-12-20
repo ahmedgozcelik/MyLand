2ff4fb851787126b59305ab201d158615945451f
Ahmed Gürkan Özçelik <ahmetgozcelik@gmail.com>
2023-12-20 13:58:14 +0300

    smoke animation in bakery shop

--- Assets/Scripts/UnLockBakeryUnitControl.cs
+++ Assets/Scripts/UnLockBakeryUnitControl.cs
@@ -13,6 +13,8 @@ public class UnLockBakeryUnitControl : MonoBehaviour
     [SerializeField] private Transform coinTransform;
     [SerializeField] private GameObject coinGO;
 
+    [SerializeField] private ParticleSystem smokeParticle;
+
     private float time;
     private int storedProductCount;
     void Start()
@@ -37,6 +39,7 @@ public class UnLockBakeryUnitControl : MonoBehaviour
     private void DisplayProductCount()
     {
         bakeryText.text = storedProductCount.ToString() + "/" + maxStoredProductCount.ToString();
+        ControlSmokeEffect();
     }
 
     public ProductType GetNeededProductType()
@@ -68,6 +71,24 @@ public class UnLockBakeryUnitControl : MonoBehaviour
         Vector3 InstantiatePos = coinTransform.position + position;
 
         Instantiate(coinGO, InstantiatePos, Quaternion.identity);
+    }
+
+    private void ControlSmokeEffect()
+    {
+        if(storedProductCount == 0)
+        {
+            if (smokeParticle.isPlaying)
+            {
+                smokeParticle.Stop();
+            }
+        }
+        else
+        {
+            if(smokeParticle.isStopped)
+            {
+                smokeParticle.Play(); 
+            }
+        }
     }
    
 }
